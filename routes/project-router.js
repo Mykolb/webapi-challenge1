1a2ad4abc4bc96bad953f05cea66c95eaac0b180
Mykol Benning <mykolbenning@MBP13inhMid2012.fios-router.home>
2019-04-26 11:46:37 -0400

    created post req for projects, is working

--- routes/project-router.js
+++ routes/project-router.js
@@ -16,11 +16,24 @@ router.get('/', (req, res) => {
         res.status(200).json(projects);
     })
     .catch(err => {
-        res.status(500).json({ error: err, message: 'Project information could not be found'})
+        res.status(500).json({ error: err, message: 'Project information could not be found.'})
     })
 })
 
+router.post('/', (req, res) => {
+    const projectInfo = req.body;
+    console.log('projectInfo');
 
+
+    projectDb
+    .insert(projectInfo)
+    .then(project => {
+        res.status(201).json(project);
+    })
+    .catch(err => {
+        res.status(500).json({ error: err, message: 'New project could not be created.'})
+    })
+})
 
 
 
